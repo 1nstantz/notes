368b319d0ec1d6e94deeabce96345f0a2e15ea64
qinhao <760051064@qq.com>
2022-03-10 11:34:02 +0800

    1

--- springboot/src/main/java/com/qinhao/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/qinhao/springboot/SpringbootApplication.java
@@ -1,15 +1,18 @@
 package com.qinhao.springboot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com/qinhao/springboot/mapper")
+@Slf4j
 public class SpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
+        log.info("服务器启动成功");
     }
 
 }
