fb3e0249dd9c657bae798afe6c01791f446dbed1
qinhao <760051064@qq.com>
2022-05-24 16:54:00 +0800

    1

--- concurrent/src/main/java/com/qinhao/future/UseCompletableFuture.java
+++ concurrent/src/main/java/com/qinhao/future/UseCompletableFuture.java
@@ -5,29 +5,16 @@ import org.junit.Test;
 import java.util.concurrent.*;
 
 /**
+ * runAsync没有返回结果
+ * supplyAsync 可以获取返回结果，get()阻塞获取返回结果
+ * AcceptAsync没有返回结果，但是可以传参。
+ *
+ *  这三个方法前面加then实现链式编程
  * @author qinhao
  * @date 2022/3/7 - 11:34
  */
 public class UseCompletableFuture {
     public ExecutorService executor = Executors.newFixedThreadPool(4);
-
-    /**
-     * 1.8提供的异步函数式编程
-     */
-    @Test
-    public void useCompletableFuture() throws Exception {
-        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
-            try {
-                TimeUnit.SECONDS.sleep(2);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            return "hello world";
-        }, executor);
-
-        System.out.println(future.get());  //阻塞的获取结果  ''helllo world"
-    }
-
 
     /**
      * 阻塞式获取结果
@@ -45,5 +32,42 @@ public class UseCompletableFuture {
         System.out.println(future.isDone());
         // get() 方法阻塞式的获取返回结果
         System.out.println(future.get());
+    }
+
+
+    /**
+     * 1.8提供的异步函数式编程
+     */
+    @Test
+    public void useCompletableFuture() throws Exception {
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "hello world";
+        }, executor).whenCompleteAsync((res,exception)->{//whenCompleteAsync可以捕获到异常信息
+            System.out.println(res);
+            System.out.println(exception);
+        },executor).exceptionally((e)->{//exceptionally对异常做处理
+            System.out.println(e);
+            return "失败";
+        });
+
+
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "hello world";
+        }, executor).handleAsync((res, ex) -> {
+            System.out.println(res);
+            return "shibai";
+        }, executor);
+        //handlerAsync就是whenCompleteAsync和exceptionally的结合
+        System.out.println(future.get());  //阻塞的获取结果  ''helllo world"
     }
 }
